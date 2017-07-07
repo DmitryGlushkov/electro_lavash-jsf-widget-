d9f1247fa79f0810b03b02cb7370e4c744d039f1
Dmitry Glushkov <glushkov.1000@gmail.com>
2017-07-07 12:44:20 +0500

    Updated

--- src/ru/electrolavash/BlurHandler.java
+++ src/ru/electrolavash/BlurHandler.java
@@ -13,13 +13,19 @@ import javax.faces.context.FacesContext;
 import javax.faces.view.BehaviorHolderAttachedObjectHandler;
 import javax.faces.view.facelets.*;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class BlurHandler extends TagHandlerImpl implements BehaviorHolderAttachedObjectHandler {
+
+    private static final String JS_FUNCTION = "blur_listener";
+    private static final String RENDER_TYPE_SCRIPT = "javax.faces.resource.Script";
+    private static final String RENDER_TYPE_STYLE = "javax.faces.resource.Stylesheet";
 
     private final TagAttribute effect;
     private final TagAttribute event;
     private final TagAttribute execute;
     private final TagAttribute render;
+    private final TagAttribute target;
 
     private final boolean wrapping;
 
@@ -29,6 +35,7 @@ public class BlurHandler extends TagHandlerImpl implements BehaviorHolderAttache
         this.effect = this.getAttribute("effect");
         this.execute = this.getAttribute("execute");
         this.render = this.getAttribute("render");
+        this.target = this.getAttribute("target");
         this.wrapping = isWrapping();
         System.out.println("[BlurHandler] constructor");
     }
@@ -49,14 +56,14 @@ public class BlurHandler extends TagHandlerImpl implements BehaviorHolderAttache
             final String eventName = bHolder.getDefaultEventName();
             final AjaxBehavior ajaxBehavior = createAjaxBehavior(ctx);
             bHolder.addClientBehavior(eventName, ajaxBehavior);
-            installAjaxResourceIfNecessary();
+            installAjax();
         } else {
             throw new TagException(this.tag, "Unable to attach <f:ajax> to non-ClientBehaviorHolder parent");
         }
 
     }
 
-    private void installAjaxResourceIfNecessary() {
+    private void installAjax() {
 
         FacesContext context = FacesContext.getCurrentInstance();
 
@@ -64,35 +71,56 @@ public class BlurHandler extends TagHandlerImpl implements BehaviorHolderAttache
             return;
         }
 
-        final UIOutput jsf = getLibOutput(context, "jsf.js", "javax.faces");
-        final UIOutput lavash = getLibOutput(context, "lavash.js", "ru.electrolavash");
+        final UIOutput jsf = getLibOutput(context, "jsf.js", "javax.faces", RENDER_TYPE_SCRIPT);
+        final UIOutput lavash = getLibOutput(context, "lavash.js", "ru.electrolavash", RENDER_TYPE_SCRIPT);
+        final UIOutput css = getLibOutput(context, "lavash.css", "ru.electrolavash", RENDER_TYPE_STYLE);
 
         if (jsf != null) context.getViewRoot().addComponentResource(context, jsf, "head");
         if (lavash != null) context.getViewRoot().addComponentResource(context, lavash, "head");
+        if (css != null) context.getViewRoot().addComponentResource(context, css, "head");
 
         RenderKitUtils.setScriptAsRendered(context);
 
     }
 
-    private UIOutput getLibOutput(final FacesContext context, final String name, final String library) {
+    private UIOutput getLibOutput(final FacesContext context, final String name, final String library, final String renderType) {
         if (RenderKitUtils.hasResourceBeenInstalled(context, name, library)) {
             RenderKitUtils.setScriptAsRendered(context);
             return null;
         }
         UIOutput output = new UIOutput();
-        output.setRendererType("javax.faces.resource.Script");
+        output.setRendererType(renderType);
         output.getAttributes().put("name", name);
         output.getAttributes().put("library", library);
         return output;
     }
 
     private AjaxBehavior createAjaxBehavior(FaceletContext ctx) {
+        checkBehaviorId();
         final Application application = ctx.getFacesContext().getApplication();
-        final AjaxBehavior behavior = (AjaxBehavior) application.createBehavior(AjaxBehavior.BEHAVIOR_ID);
+        final BlurBehavior behavior = (BlurBehavior) application.createBehavior(BlurBehavior.BEHAVIOR_ID);
         setBehaviorAttribute(ctx, behavior, this.execute, Object.class);
         setBehaviorAttribute(ctx, behavior, this.render, Object.class);
-        setBehaviorAttribute(ctx, behavior, new TagAttributeImpl(render.getLocation(), render.getNamespace(), "onevent", "onevent", "blur_listener"), String.class);
+        setBehaviorAttribute(ctx, behavior, new TagAttributeImpl(render.getLocation(), render.getNamespace(), "onevent", "onevent", JS_FUNCTION), String.class);
         return behavior;
+    }
+
+    private void checkBehaviorId() {
+        if (!isBehaviorIdAdded()) {
+            final Application application = FacesContext.getCurrentInstance().getApplication();
+            application.addBehavior(BlurBehavior.BEHAVIOR_ID, BlurBehavior.class.getCanonicalName());
+        }
+    }
+
+    private boolean isBehaviorIdAdded() {
+        final Application application = FacesContext.getCurrentInstance().getApplication();
+        final Iterator<String> idIterator = application.getBehaviorIds();
+        while (idIterator.hasNext()) {
+            if (idIterator.next().equals(BlurBehavior.BEHAVIOR_ID)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void setBehaviorAttribute(FaceletContext ctx, AjaxBehavior behavior, TagAttribute attr, Class type) {
@@ -103,9 +131,14 @@ public class BlurHandler extends TagHandlerImpl implements BehaviorHolderAttache
 
     @Override
     public String getEventName() {
-        FacesContext context = FacesContext.getCurrentInstance();
-        FaceletContext ctx = (FaceletContext) context.getAttributes().get(FaceletContext.FACELET_CONTEXT_KEY);
+        FaceletContext ctx = getFaceletContext();
         return (this.event != null) ? this.event.getValue(ctx) : null;
+    }
+
+    private FaceletContext getFaceletContext() {
+        final FacesContext context = FacesContext.getCurrentInstance();
+        FaceletContext ctx = (FaceletContext) context.getAttributes().get(FaceletContext.FACELET_CONTEXT_KEY);
+        return ctx;
     }
 
     @Override
--- web/resources/ru.electrolavash/lavash.js
+++ web/resources/ru.electrolavash/lavash.js
@@ -1,16 +1,71 @@
+
+
+var B = function () {
+    this.start = function () {
+        console.log("start");
+    }
+    this.stop = function () {
+        console.log("stop");
+    }
+}
+
 function blur_listener(data) {
-    var status = data.status; // Can be "begin", "complete" or "success".
+    var status = data.status;
     switch (status) {
-        case "begin": // Before the ajax request is sent.
-            console.log('begin');
+        case "begin":
+            console.log("begin");
+            //B.start();
             break;
-
-        case "complete": // After the ajax response is arrived.
-            console.log('complete');
+        case "complete":
+            console.log("complete");
+            //B.stop();
             break;
-
-        case "success": // After update of HTML DOM based on ajax response..
+        case "success":
             console.log('success');
             break;
     }
+}
+
+function myMove() {
+    var elem = document.getElementById("animate");
+    var pos = 0;
+    var id = setInterval(frame, 5);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }
+    }
+}
+
+
+function blur() {
+    console.log('-> blur');
+    //document.body.className += " blur";
+    //document.body.classList.add("blur");
+    var body = document.body;
+    body.style.setProperty('filter', 'blur(5px)');
+
+    var id = setInterval(frame, 5);
+    var i = 0;
+    function frame() {
+        i++;
+        console.log(i);
+        /*if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++;
+            elem.style.top = pos + 'px';
+            elem.style.left = pos + 'px';
+        }*/
+    }
+
+}
+
+function unblur() {
+    console.log('-> unblur');
+    //document.body.classList.remove("blur");
 }
